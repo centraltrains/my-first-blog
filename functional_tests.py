dfebc54bba8483303333cbdff0235fbe64617d80
Benjamin Roughton <bfr859@student.bham.ac.uk>
2020-08-10 21:20:42 +0100

    edit button test and dummy

--- functional_tests.py
+++ functional_tests.py
@@ -16,12 +16,36 @@ class SiteTest(unittest.TestCase):
         table = self.browser.find_element_by_id(id)
         rows = table.find_elements_by_tag_name("tr")
         self.assertIn(text, table.text)
+    
+    def find_row_in_table(self, text, id):
+        table = self.browser.find_element_by_id(id)
+        rows = table.find_elements_by_tag_name("tr")
+        for row in rows:
+            if text in row.text:
+                return row
+
+        return None
+
+    def get_link_from_listing(self, text, id):
+        row = self.find_row_in_table(text, id)
+        if row != None:
+            links = row.find_elements_by_tag_name("a")
+            for link in links:
+                return link.get_attribute("href")
+        
+        return None
+
 
     def test_website_connect(self):
 
         #Connect to website
         self.browser.get("http://127.0.0.1:8000/admin")
-        time.sleep(10)
+        username = self.browser.find_element_by_id("id_username")
+        password = self.browser.find_element_by_id("id_password")
+        username.send_keys("test")
+        password.send_keys("testworld")
+        password.send_keys(Keys.ENTER)
+        time.sleep(1)
         self.browser.get("http://127.0.0.1:8000/cv")
 
         #Check it is the right website
@@ -44,6 +68,11 @@ class SiteTest(unittest.TestCase):
 
         #Check
         self.check_text_in_id("GCSEs: A*A*A*AABCD", "education-table")
+
+        #Edit
+        editLink = self.get_link_from_listing("GCSEs: A*A*A*AABCD", "education-table")
+        self.assertIsNotNone(editLink)
+        self.browser.get(editLink)
 
         self.fail("Finish the test")
 
